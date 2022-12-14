c9016d48f30d08a7c79c616d794f95d265168ab2
Omoseke Salami <omosekesalami@gmail.com>
2022-12-14 09:01:09 -0800

    added sub function

--- stack_functions_2.c
+++ stack_functions_2.c
@@ -19,6 +19,25 @@ void add(stack_t **top, __attribute__((unused))unsigned int n)
 	pop(top, lineNum);
 }
 /**
+ * sub - sub bewtween top
+ * @top: top of the stack
+ * @n: the value on the top
+ */
+void sub(stack_t **top, __attribute__((unused))unsigned int n)
+{
+	int temp = 0;
+
+	if (!(*top) || !(*top)->next)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", lineNum);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*top)->n;
+	(*top)->next->n = (*top)->next->n - temp;
+	pop(top, lineNum);
+}
+/**
  * _div - divide bewtween top
  * @top: top of the stack
  * @n: the value on the top
